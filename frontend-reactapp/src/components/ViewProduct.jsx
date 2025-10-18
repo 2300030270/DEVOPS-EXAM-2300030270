689125e43095011d6b8533d8e9af17485799a3ae
unknown <2300030270@kluniversity.in>
2025-10-18 11:03:46 +0530

    Updated V3.0

--- frontend-reactapp/src/components/ViewProduct.jsx
+++ frontend-reactapp/src/components/ViewProduct.jsx
@@ -5,7 +5,7 @@ import './style.css'
 
 
 const BASE_URL = import.meta.env.VITE_API_URL;
-const API_URL = `${BASE_URL}`;
+const API_URL = `${BASE_URL}/productapi`;
 
 function ViewProduct() {
   const { id } = useParams();
